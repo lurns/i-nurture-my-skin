55706126428ec8d04203b594c9f40e4f5fae4240
Lauren Lee <lrnlee08@gmail.com>
2026-09-06 14:17:23 -0500

    base icon file

---
+++ src/lib/utils/icons.ts
@@ -0,0 +1,11 @@
+export const calendarIcon: string = 'icon-[akar-icons--calendar]';
+export const queueIcon: string = 'icon-[akar-icons--receipt]';
+
+export const addIcon: string = 'icon-[akar-icons--circle-plus]';
+export const editIcon: string = 'icon-[akar-icons--edit]';
+export const deleteIcon: string = 'icon-[akar-icons--trash-can]';
+
+export const timeUsedIcons: Record<string, string> = {
+  daytime: 'icon-[akar-icons--sun-fill]',
+  nighttime: 'icon-[akar-icons--moon-fill]'
+}
